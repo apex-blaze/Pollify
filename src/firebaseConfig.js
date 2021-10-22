095a230b0fdc6e988d4aa69a11ef47583b52bb58
apex-blaze <59483233+apex-blaze@users.noreply.github.com>
2021-10-22 15:52:32 +0530

    Set up firebase config file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAQVrCaPJswzjixjqZTeWC22f7rWdT-CZk",
+  authDomain: "pollify-343ee.firebaseapp.com",
+  projectId: "pollify-343ee",
+  storageBucket: "pollify-343ee.appspot.com",
+  messagingSenderId: "132338889311",
+  appId: "1:132338889311:web:1517aea6bc3509f1f9a1ea",
+  measurementId: "G-LM2TXZJN73",
+};
