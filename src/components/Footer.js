d384d1ddb3dcc16e660b90174cd2743205b83d2f
apex-blaze <59483233+apex-blaze@users.noreply.github.com>
2021-11-03 14:46:23 +0530

    Changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="mt-auto">
-      <div className="w-full py-4 shadow-lg bg-purple-bright text-white rounded-t-lg">
+      <div className="w-full py-4 shadow-lg bg-white text-gray-500 rounded-t-lg">
         <p>
-          Made with <i className="fas fa-heart"></i> by Ritish
+          Made with <i className="fas fa-heart text-pink-600"></i> by{" "}
+          <span className="text-purple-bright">2022 Batch</span>
         </p>
       </div>
     </div>
